acc410b1eec2998f4369ee1a2d72ae7be036ecab
Lucas Assis <lucasrafaeisaac@gmail.com>
2024-08-20 18:28:23 -0300

    mensagens obrigatórias

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -36,6 +36,29 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     it('campo telefone continua vazio como preencher com valor não-númerico', function() {
         cy.get('#phone').type('abcdefghaikhkd').should('have.value', '')
     });
+
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+        cy.get('#firstName').type("Luicas")
+        cy.get('#lastName').type('Assis')
+        cy.get('#email').type('lucas@exemplo.com')
+        cy.get('#phone-checkbox').click()
+        cy.get('#open-text-area').type('test')
+        cy.get('button[type="Submit"]').click()
+
+        cy.get('.error').should('be.visible')
+        
+    });
+
+    it.only('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+        cy.get('#firstName').type("Lucas").should('have.value','Lucas').clear().should('have.value', "")
+        cy.get('#lastName').type('Assis').should('have.value','Assis').clear().should('have.value', "")
+        cy.get('#email').type('lucas@exemplo.com').should('have.value','lucas@exemplo.com').clear().should('have.value', "")
+        cy.get('#phone').type('81999223344').should('have.value', '81999223344').clear().should('have.value','')
+
+
+        
+    });
+
   })
 
 
