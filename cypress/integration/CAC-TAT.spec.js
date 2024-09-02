7d1a3a8b05b3d92df3fe4451e1731ae55a3188c2
LucaDAssis <lucasrafaeisaac@gmail.com>
2024-09-02 17:41:36 -0300

    read.me

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -135,7 +135,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     });
 
     //acessando a pagina removendo o tanget e clicando no link assim podendo usar ele no cypress
-    it.only('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+    it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
         cy.get('#privacy a')
         .invoke('removeAttr', 'target')
         .click()
